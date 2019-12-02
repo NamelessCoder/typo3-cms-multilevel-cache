5468a1bbf0250356dbca0630ad6a380503ed46df
Claus Due <claus@namelesscoder.net>
2019-12-02 13:17:10 +0100

    [FEATURE] Declare support for TYPO3v10 + dev-master

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'php' => '7.0.0-7.2.99',
-      'typo3' => '7.6.0-9.5.99',
+      'php' => '7.0.0-7.4.99',
+      'typo3' => '7.6.0-10.2.99',
     ),
     'conflicts' =>
     array (
